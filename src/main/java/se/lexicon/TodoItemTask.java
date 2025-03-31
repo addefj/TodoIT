a19509272ada9ac75e03931487b15fc8e11b51fa
addefj <adde.fj@gmail.com>
2025-03-31 22:42:39 +0200

    TodoItemTask class part 1 finished

--- src/main/java/se/lexicon/TodoItemTask.java
+++ src/main/java/se/lexicon/TodoItemTask.java
@@ -2,5 +2,58 @@ package se.lexicon;
 
 public class TodoItemTask {
 
+    //fields
+    private int id;
+    private boolean assigned;
+    private TodoItem todoItem;
+    private Person assignee;
 
+    //constructor
+    public TodoItemTask(int id, TodoItem todoItem, Person assignee) {
+        this.id = id;
+        setTodoItem(todoItem);
+        setAssignee(assignee);
+    }
+
+    //getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public boolean isAssigned() {
+        return assigned;
+    }
+
+    public TodoItem getTodoItem() {
+        return todoItem;
+    }
+
+    public Person getAssignee() {
+        return assignee;
+    }
+
+    public void setTodoItem(TodoItem todoItem) {
+        if(todoItem == null){
+            throw new IllegalArgumentException("todoItem can't be null");
+        }
+        this.todoItem = todoItem;
+    }
+
+    public void setAssignee(Person assignee) {
+        if(assignee != null){
+            this.assigned = true;
+        }
+        this.assignee = assignee;
+    }
+
+    //other methods
+    public String getSummary(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Id: ").append(id).append(", TodoItem: ")
+                .append(todoItem).append(", Assignee: ")
+                .append(assignee).append(", Assigned: ")
+                .append(assigned);
+
+        return sb.toString();
+    }
 }
