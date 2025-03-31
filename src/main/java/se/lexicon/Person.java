3c87223b2efc83b6bbb2a25a5184e3da966da47a
addefj <adde.fj@gmail.com>
2025-03-31 21:33:23 +0200

    Person class part 1 finished

--- src/main/java/se/lexicon/Person.java
+++ src/main/java/se/lexicon/Person.java
@@ -1,4 +1,68 @@
 package se.lexicon;
 
 public class Person {
+
+    //fields
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    //constructor
+    public Person(int id, String firstName, String lastName, String email){
+    this.id = id;
+    setFirstName(firstName);
+    setLastName(lastName);
+    setEmail(email);
+    }
+
+    //getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setFirstName(String firstName) {
+        if(firstName == null || firstName.isEmpty()){
+            throw new IllegalArgumentException("firstname can't be null or empty");
+        }
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        if(lastName == null || lastName.isEmpty()){
+            throw new IllegalArgumentException("lastname can't be null or empty");
+        }
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        if(email == null || email.isEmpty()){
+            throw new IllegalArgumentException("email can't be null or empty");
+        }
+        this.email = email;
+    }
+
+    //other methods
+    public String getSummary(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Id: ").append(id).append(", Name: ")
+                .append(firstName).append(" ").append(lastName)
+                .append(", email: ").append(email);
+
+        return sb.toString();
+    }
+
 }
